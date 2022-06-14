d51cc8c414218bcf554820d5ffcbf69ef14a8479
Fabien <fabienswift@gmail.com>
2022-06-14 18:25:37 -0400

    Fix errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./style.css";
-import {getScore} from "./scores.js";
+import './style.css';
+import getScore from './scores.js';
 
-getScore()
+getScore();
--- src/scores.js
+++ src/scores.js
@@ -1,22 +1,22 @@
 const scores = [
   {
-    name: "Name",
-    score: "100",
+    name: 'Name',
+    score: '100',
   },
   {
-    name: "Name",
-    score: "50",
+    name: 'Name',
+    score: '50',
   },
   {
-    name: "Name",
-    score: "20",
+    name: 'Name',
+    score: '20',
   },
 ];
 
-
-export const getScore = () => {
+const getScore = () => {
   scores.forEach((score) => {
-    const scoreList = `<li class="score-list"><label id="" for="" class="">${score.name}: ${score.score}</label></li>`;
-    document.getElementById("leaderboard").innerHTML += scoreList;
+    const scoreList = `<li class="score-list"><label id="label" for="score" class="score-slot">${score.name}: ${score.score}</label></li>`;
+    document.getElementById('leaderboard').innerHTML += scoreList;
   });
 };
+export default getScore();
